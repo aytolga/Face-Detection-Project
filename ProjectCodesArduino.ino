7584777674da08209ddee062c73744f9a5d38e47
Tolga AY <ecetolgaay@gmail.com>
2021-12-30 13:56:09 +0300

    Update ProjectCodesArduino.ino

--- ProjectCodesArduino.ino
+++ ProjectCodesArduino.ino
@@ -2,7 +2,7 @@
 #include "cvzone.h"    
 #include "Servo.h"    //Implementatin of some neccessary libraries.
 Servo servo;          //Definiton of servo motor.               
-SerialData data(2,3); // To create the serial object to recieve some values from Python.                        to recieve some values from Python */
+SerialData data(2,3); // To create the serial object to recieve some values from Python.                       
 int value[10];        // An array for stored values.
 int light = 10;
 void setup () {
@@ -15,8 +15,8 @@ void setup () {
 void loop () {
   data.Get(value);     //Starting to getting values from Python    
   servo.write(value[0]);  //Servo motor commands  
-  delay(100);
+  delay(0);
   digitalWrite(light,value[0]); //LED commands
-  delay(100);
+  delay(0);
   
 }
